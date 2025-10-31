7dda67dac28ae165d2cc0d2b1eb29c9271c5f303
samyfg41 <samanthafaruolo@gmail.com>
2025-10-31 11:03:37 -0300

    Atualizando

--- src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
+++ src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
@@ -14,7 +14,7 @@ public class ConexaoFactory {
 
         // Retornar conexão
         return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl",
-              "usuario", "senha");
+              "RM554794", "fiap25");
     }
 
 }
